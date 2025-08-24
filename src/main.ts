60405c11217fd8f141fdec6294cd5be154dd2814
Joao Victor <vektor5665@hotmail.com>
2025-08-24 08:21:19 -0300

    CORS rote

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: 'https://crm-gamma-red.vercel.app/' });
   app.use(cookieParser());
   await app.listen(3001);
 }
